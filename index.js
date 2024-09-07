ca03db68e32690dc851e24814fcf6497a9f3da26
Rakesh Arora <mannyisha808@gmail.com>
2024-09-07 23:46:39 +0530

    013

--- index.js
+++ index.js
@@ -1,53 +1,49 @@
-import express , {Router} from "express";
-import serverless from "serverless-http";
-const app  = express();
+import express from "express";
+const app = express();
 
 import puppeteer from "puppeteer";
 
 const browser = await puppeteer.launch({
-    headless:true,
-    defaultViewport:null
+    headless: true,
+    defaultViewport: null
 });
 
 const page = await browser.newPage();
-
-const router = Router();
-
-router.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     return res.send("This app is made by Mohit - for more information contact 9667067062");
 });
 
-router.get("/order/track/",async (req,res)=>{
+app.get("/order/track/", async (req, res) => {
     try {
         await page.goto(`https://shipeasy.tech/app/public/tracking/${req.query.awb}`, { waitUntil: "domcontentloaded" });
 
         const data = await page.evaluate(() => {
             let ret = [];
             const tr = document.getElementsByTagName("tr");
-            for(let i=0;i<tr.length;i++){
+            for (let i = 0; i < tr.length; i++) {
                 const th = tr[i].children;
                 let dt = [];
-                for(let j=0;j<th.length;j++){
+                for (let j = 0; j < th.length; j++) {
                     dt.push(th[j].innerHTML);
                 }
                 ret.push(dt);
             }
             return ret
         })
-        if(data.length == 0){
+        if (data.length == 0) {
             return res.status(404).send("Awb not found");
         }
 
         return res.send(data)
     } catch (e) {
-        res.json({ error: true, message:e})
+        res.json({ error: true, message: e })
     }
 })
 
-router.use((req,res)=>{
+app.use((req, res) => {
     return res.status(404).send("Page not found")
 })
 
-app.use('/.netlify/functions/index', router);
-
-export default serverless(app);
+app.listen(4000, () => {
+    console.log("Listing on port 4000")
+})
